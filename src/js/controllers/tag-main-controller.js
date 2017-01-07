c6440a689cf4826409941d0391a8ed26d174a1b7
Lucas Santos <lhs.santoss@gmail.com>
2017-01-07 01:49:06 -0200

    Created view model for tag module

--- src/js/controllers/tag-main-controller.js
+++ src/js/controllers/tag-main-controller.js
@@ -3,8 +3,13 @@
 
   angular.module('tagModule')
     .controller('tagMainController', tagMainController);
-  
-  function tagMainController($scope, $http) {
 
+  function tagMainController($scope, $http) {
+    $scope.photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    $scope.image_size = 500;
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 })();
