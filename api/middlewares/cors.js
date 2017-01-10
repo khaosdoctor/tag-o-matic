66cdd16a6aa78dcbcf727e26cdc7e9f513af6318
Lucas Santos <lhs.santoss@gmail.com>
2017-01-09 22:58:57 -0200

    Added new cors methods

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Accept,Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
   next();
 };
